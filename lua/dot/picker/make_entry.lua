6df8e1e4b1ca637ea3495ad4a3406066d92eee1d
Jorge Morales <jorge.iv.morales@gmail.com>
2024-03-31 19:45:02 +0200

    chore: move icon to dedicated column

--- lua/dot/picker/make_entry.lua
+++ lua/dot/picker/make_entry.lua
@@ -1,4 +1,5 @@
 local entry_display = require('telescope.pickers.entry_display')
+local strings = require('plenary.strings')
 local shortcut = require('dot.shortcut')
 local M = {}
 
@@ -16,22 +17,25 @@ function M.from_story(story)
       return nil
     end
 
+    local icon = story_type_icon_map[entry.value.story_type]
+
     -- Create a displayer with two columns:
     local displayer = entry_display.create({
       separator = ' ',
       items = {
         -- Story id column
         { width = 8 },
+        -- Story type icon
+        { width = strings.strdisplaywidth(icon) },
         -- Story name column
         { remaining = true },
       },
     })
 
-    local icon = story_type_icon_map[entry.value.story_type] or ''
-
     return displayer({
       { entry.value.id, 'TelescopeResultsNumber' },
-      { icon .. ' ' .. entry.value.name },
+      { icon },
+      { entry.value.name },
     })
   end
 
